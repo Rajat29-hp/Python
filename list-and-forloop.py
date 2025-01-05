1145fb8ec91e797f130164b503e88711ac040fcd
Rajat29-hp <kumar.rajat0978@gmail.com>
2025-01-05 20:13:03 +0530

    list , Dictionary , For loop

---
+++ list-and-forloop.py
@@ -0,0 +1,30 @@
+# List
+  mylist= ['bob','jack','rock',1001,1.1]
+  print(mylist)
+
+ # pop
+     mylist.pop(3)
+ # appent
+     mylist.append('john')
+
+# -------------------------------------------
+
+# Dictionary , key: value
+    mydist = {'userdata':'rajat','id':'1001'}
+    print(mydist)
+
+# Check in dictionary key and value
+   myDist.values()
+   myDist.keys()
+
+# -------------------------------------------
+  #Tuple
+     rare_list = [(1,3),(23,51),(78,95),(78,95),(15,25)]
+     print(x)
+     print(y)
+# -------------------------------------------
+
+  # For loop
+      for user in mylist:
+        print(user)
+
