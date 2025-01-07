6c811b1d3a9c7fc67e5abd7ca552d62229f354ba
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-01-07 19:06:29 +0530

    Create if-elif

---
+++ if-elif.py
@@ -0,0 +1,24 @@
+# if and else
+
+print("----------------------------")
+  x = input("Enter a value in between 1 to 3\n")
+  if x ==2:
+    print("you entered 2 value")
+  else:
+    print("try again")
+
+#-----------------------------------------  
+
+# if and else
+print("----------------------------")
+  x = input("Enter a value in between 1 to 4\n")
+  if x ==1:
+    print("you entered value is 1")
+  elif x==2:
+    print("you entered value is 2")
+  elif x==3:
+    print("you entered value is 3")
+  elif x==4:
+    print("you entered value is 4")
+else:
+  print("Entered value is incorrect")
