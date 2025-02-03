36dee853f102246a0f47d9daf6b710cc35036fd0
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-02-03 19:29:14 +0530

    Update s3-bucket.py

--- aws-s3/s3-bucket.py
+++ aws-s3/s3-bucket.py
@@ -18,3 +18,19 @@ def create_and_list_bucket(bucket_name=None,bucket_region='eu-north-1'):
 
 create_and_list_bucket(bucket_name='python')
 
+create_and_list_bucket(bucket_name='pythonmy4')
+
+def copy_to_bucket(file_path,bucket_name):
+        s3 = boto3.client('s3')
+        filename = file_path.split('/')[-1]
+        s3.upload_file(file_path,bucket_name,filename)
+        print(f"{file_path} ---> {bucket_name}")
+        response = s3.list_objects_v2(Bucket=bucket_name)
+        print(f"\nContents of {bucket_name}")
+        n = 0
+        for obj in response['Contents']:
+            n  = n + 1
+            print(f"{n}.{obj['Key']}")
+
+
+copy_to_bucket('/etc/hosts','pythonmy4')
