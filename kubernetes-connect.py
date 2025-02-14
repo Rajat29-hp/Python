58497d627963b5cb19e7a782a329d23b303f852f
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-02-14 21:01:50 +0530

    Update kubernetes-connect.py

--- kubernetes-connect.py
+++ kubernetes-connect.py
@@ -1,3 +1,4 @@
+from kubernetes import client,config
 import os
 import urllib3
 
@@ -26,6 +27,7 @@ class K8Info():
                 print(f"Total Nodes: {len(nodes)}")
                for node in nodes:
 		       print(f"Name: {node.metadata.name}")
+		       
 		       print(f"\tStatus: {node.status.conditions[-1].type}")
 		       print(f"\tStatus: {node.status.capacity['cpu']}")
 		       print(f"\tStatus: {node.status.capacity['memory']}")
@@ -34,12 +36,26 @@ class K8Info():
            print(f"Total Namespaces: {len(namespaces)}")
            print(f"==============list the namespaces=========================")
            for ns in namespaces:
-               print(f"Name: {node.metadata.name}")
+		   print(f"Name: {node.metadata.name}")
+		   
+		   pods = self.core_v1.list_namespaced_pod(ns_name).items
+		   services = self.core_v1.list_namespaced_service(ns_name).items
+		   deployments = apps.core_v1.list_namespaced_deployment(ns_name).items
+		   print(f"\t Total Number of Pod are running : {len(pods)}")
+		   print(f"\t Total Number of services are running : {len(services)}")
+		   print(f"\t Total Number of deployment are running : {len(deployments)}")
+		    print(f"==============Running Pod=========================")
+
+		   for pod in pods:
+			   if pod.status.phase = "Running":
+				   print(f"Running Pod : {pod.metadata.name}")
+
 	   except Exception as e:
-	       except Exception as e:
+	       print(e)
 def main():
     cluster = K8Info(verify_ssl=false))
     cluster.get_cluster_info()
+
 if __name__ = "__main__"
     main()
  
