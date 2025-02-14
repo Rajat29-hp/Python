6a288e477ff18e154612c4334642183affaf8eac
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-02-14 20:20:20 +0530

    Update kubernetes-connect.py

--- kubernetes-connect.py
+++ kubernetes-connect.py
@@ -22,3 +22,25 @@ class K8Info():
    def get_cluster_info():
        print(f"============connectinng Cluster Information")
        try:
+	        nodes = self.core_v1.list_node().items
+                print(f"Total Nodes: {len(nodes)}")
+               for node in nodes:
+		       print(f"Name: {node.metadata.name}")
+		       print(f"\tStatus: {node.status.conditions[-1].type}")
+		       print(f"\tStatus: {node.status.capacity['cpu']}")
+		       print(f"\tStatus: {node.status.capacity['memory']}")
+
+           namespaces = self.core_v1.list_namespce().items
+           print(f"Total Namespaces: {len(namespaces)}")
+           print(f"==============list the namespaces=========================")
+           for ns in namespaces:
+               print(f"Name: {node.metadata.name}")
+	   except Exception as e:
+	       except Exception as e:
+def main():
+    cluster = K8Info(verify_ssl=false))
+    cluster.get_cluster_info()
+if __name__ = "__main__"
+    main()
+ 
+
