7c1b06bfb326ac6cebfe0faf96d62f43386c07e0
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-03-21 11:26:45 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,26 +1,26 @@
 #1st method
 print("----------------------------")
-username = input("Enter your  username\n")
-userid = input("enter your id\n")
+username = input("Enter your  username\n");
+userid = input("enter your id\n");
 
-print("You entered usermane is :",username)
-print("You entered userid is :",userid)
-print("----------------------------")
+print("You entered usermane is :",username);
+print("You entered userid is :",userid);
+print("----------------------------");
 
 
 #dot method
-print("----------------------------")
-username = input("Enter your  username\n")
-userid = input("enter your id\n")
+print("----------------------------");
+username = input("Enter your  username\n");
+userid = input("enter your id\n");
 
-print("User {} created with ID {} ",format(username,userid))
-print("----------------------------")
+print("User {} created with ID {} ",format(username,userid));
+print("----------------------------");
 
 
 # F method, it support python3
-print("----------------------------")
-username = input("Enter your  username\n")
-userid = input("enter your id\n")
+print("----------------------------");
+username = input("Enter your  username\n");
+userid = input("enter your id\n");
 
-print(f"User {username} created with ID {userid}")
-print("----------------------------")
+print(f"User {username} created with ID {userid}");
+print("----------------------------");
